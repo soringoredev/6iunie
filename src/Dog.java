6961e3911ce3b0338aa4bc23a530394ca03534f4
Sorin Gore <soringoredev@gmail.com>
2025-06-06 20:25:19 +0300

    Last changes

---
+++ src/Dog.java
@@ -0,0 +1,21 @@
+public class Dog {
+
+    String rasa = null;
+    String nume = null;
+
+
+    public Dog(String rasa, String nume) {
+        this.nume = nume;
+        this.rasa = rasa;
+    }
+
+
+    public static void main(String[] args) {
+
+        Dog numeDog  = new Dog("dalmatian", "Rex");
+        Dog numeDog2 = new Dog("ciobanesc", "Boss");
+        Dog numeDog3 = new Dog("alabai", "MyBoy");
+        Dog numeDog3 = new Dog("alabai", "MyBoy");
+
+    }
+}
